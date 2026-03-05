77ff20b7182cbb4e9e6b3e092aa313fe023c79e7
youssefAshraf21 <ya614962@gmail.com>
2026-03-06 00:15:13 +0200

    Add About and Menu sections to index.html; enhance styling in style.css and implement scroll header effect in main.js

--- main.js
+++ main.js
@@ -21,3 +21,11 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+    const scrollHeader = () =>{
+        const header = document.getElementById('header');
+        this.scrollY >= 50 ? header.classList.add('scroll-header')
+                            : header.classList.remove('scroll-header');
+    }
+    window.addEventListener('scroll', scrollHeader);
+
+    
