9f7fb1d0eeffe3241afd089ecf761a85e4289026
youssefAshraf21 <ya614962@gmail.com>
2026-03-11 21:19:19 +0200

    Add contact section with subscription form and footer; enhance styles for responsiveness

--- main.js
+++ main.js
@@ -46,3 +46,42 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 });
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 1500,
+    delay: 300,
+    easing: 'cubic-bezier(0.34, 1.56 , 0.64, 1)',
+});
+
+sr.reveal('.home__title', {origin: 'top'})
+sr.reveal('.home__description', {delay: 600, origin: 'top'})
+sr.reveal('.home__data .button', {delay: 900, distance: 0, scale: 0, origin: 'top'})
+sr.reveal('.home__blob', {delay: 900, distance: '100px'})
+sr.reveal('.home__images img', {delay: 1200, distance: 0, interval: 200, scale: 0})
+sr.reveal('.home__data img', {delay: 1500, distance: 0, interval: 200, scale: 0})
+sr.reveal('.home__text', {delay: 2100, distance: '120px'})
+
+sr.reveal('.about__data .section__title')
+sr.reveal('.about__data .section__subtitle', {delay: 600})
+sr.reveal('.about__description', {delay: 900})
+sr.reveal('.about__data .button', {delay: 1200, distance: 0, scale: 0})
+sr.reveal('.about__blob', {delay: 1200, origin: 'left'})
+sr.reveal('.about__img', {delay: 1500, origin: 'right'})
+sr.reveal('.about__data img', {delay: 1800, distance: 0, interval: 200, scale: 0})
+sr.reveal('.about__text', {delay: 2100})
+
+sr.reveal('.menu__button', {interval: 100})
+sr.reveal('.menu__content', {delay: 600})
+sr.reveal('.menu__text-1', {delay: 900, origin: 'top'})
+sr.reveal('.menu__text-2', {delay: 1200})
+
+sr.reveal('.new__data .section__title')
+sr.reveal('.new__data .section__subtitle', {delay: 600})
+sr.reveal('.new__description', {delay: 900})
+sr.reveal('.new__blob', {delay: 900, origin: 'right'})
+sr.reveal('.new__images img', {delay: 1200, distance: 0, interval: 200, scale: 0})
+sr.reveal('.new__data img', {delay: 1500, distance: 0, interval: 200, scale: 0})
+sr.reveal('.new__text-1', {delay: 2100})
+sr.reveal('.new__text-2', {delay: 2400})
